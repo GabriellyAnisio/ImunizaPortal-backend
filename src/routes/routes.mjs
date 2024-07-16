f10ef4e8fdd11355b455507d17b7aed6934e6321
Maria Gabrielly <mgabriellyanisio@gmail.com>
2024-07-16 18:40:49 -0300

    :sparkles: feat: Adding appointment.controller - function destroy

--- src/routes/routes.mjs
+++ src/routes/routes.mjs
@@ -9,5 +9,6 @@ routes.post('/api/appointment', (request, response) => appointmentController.sto
 routes.get('/api/appointment', (request, response) => appointmentController.index(request, response));
 routes.get('/api/appointment/:id', (request, response) => appointmentController.getOne(request, response));
 routes.put('/api/appointment/:id', (request, response) => appointmentController.update(request, response));
+routes.delete('/api/appointment/:id', (request, response) => appointmentController.destroy(request, response));
 
 export default routes;
